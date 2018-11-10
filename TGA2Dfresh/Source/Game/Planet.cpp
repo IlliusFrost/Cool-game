cbbd5a1736e2142342fec6f3f65d7d5e1b4c8629
Emil Hylén Stuparich de la Barra <illiusssm@gmail.com>
2018-11-10 13:30:25 +0100

    New collider constructor

--- TGA2Dfresh/Source/Game/Planet.cpp
+++ TGA2Dfresh/Source/Game/Planet.cpp
@@ -15,7 +15,7 @@ void Planet::Init(const char* aString, Vector2f aPos)
 {
 	mySprite = new Tga2D::CSprite(aString);
 	mySprite->SetPivot({ 0.5f, 0.5f });
-	myCollider = new CircleCollider(myPos, 3);
+	myCollider = new CircleCollider(myPos, 3, CollisionFlag::ePlanet);
 	myPos = aPos;
 	mySprite->SetPosition({ myPos.x, myPos.y });
 }
