3bd024c84bca94c4efe8984bc0e5314373c361fd
noCucks <34436753+noCucks@users.noreply.github.com>
2018-11-11 08:13:37 +0100

    up

--- TGA2Dfresh/Source/Game/ColliderManager.cpp
+++ TGA2Dfresh/Source/Game/ColliderManager.cpp
@@ -48,7 +48,7 @@ void ColliderManager::Update(float aDt)
 	for (unsigned short i = 0; i < myColliders.Size(); i++)
 	{
 		if (myShouldRenderColliders)
-			Tga2D::CDebugDrawer::DrawCircle({ myColliders[i]->myPos.x, myColliders[i]->myPos.y }, myColliders[i]->myRadius);
+			//Tga2D::CDebugDrawer::DrawCircle({ myColliders[i]->myPos.x, myColliders[i]->myPos.y }, myColliders[i]->myRadius);
 		for (unsigned short j = 0; j < myColliders.Size(); j++)
 		{
 			if (myColliders[i] != myColliders[j] && !myColliders[i]->myObjectData->isMarkedForDelete && !myColliders[j]->myObjectData->isMarkedForDelete)
